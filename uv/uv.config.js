f6463a9b3e76c26a682a78616eafe2f575a2c5dd
sss <89742348+sauc23@users.noreply.github.com>
2023-10-27 14:48:03 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://dd-neon-chi.vercel.app/bare/",
+  bare: "https://cxfdfdfd.ddnsking.com/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
